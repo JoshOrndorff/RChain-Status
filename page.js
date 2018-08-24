6d3ef1f99fafe4600871dc1764cd818bc55a9294
Joshy Orndorff <admin@joshorndorff.com>
2018-08-24 17:45:51 -0400

    Minor code cleanups, can't yet get status back from blockchain

--- page.js
+++ page.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // Include the RChain-API (I symlinked it in my node_modules folder)
-const {RNode, RHOCore, logged} = require("RChain-API")
+const {RNode, RHOCore, logged} = require("rchain-api")
 
 // Need electron-specific version of grpc
 // https://stackoverflow.com/a/45016752
@@ -61,29 +61,26 @@ window.addEventListener("DOMContentLoaded", () => {
     var ack = "testAck"//Math.random().toString(36).substring(7)
     console.log("ack is " + ack)
 
-    // This shouldn't actually require a transaction, for publically visible data, right?
+    // This shouldn't actually require a transaction, for publicly visible data, right?
     // Check the status, sending it to the ack channel
     var code = `@["${userName}", "check"]!("${ack}")`
     console.log(code)
-    myNode.doDeploy({
+    var response = myNode.doDeploy({
       term: code,
       timestamp: new Date().valueOf()
     }).then(result => {
       if (!result.success) { throw(result) }
-      myNode.createBlock()
+      return myNode.createBlock()
+    }).then(_ => {
+      // Get the data from the node
+      myNode.listenForDataAtName(ack)
+      .then((blockResults) => {
+        console.log("blocks received: " + blockResults.length)
+        var lastBlock = blockResults.slice(-1).pop()
+        var lastDatum = lastBlock.postBlockData.slice(-1).pop()
+        statusP.innerHTML = RHOCore.toRholang(lastDatum)
+      })
     }).catch(oops => { console.log(oops); })
-
-    // Get the data from the node
-    myNode.listenForDataAtName(ack)
-    .then((blockResults) => {
-      console.log("blocks received: " + blockResults.length)
-      var lastBlock = blockResults.slice(-1).pop()
-      var lastDatum = lastBlock.postBlockData.slice(-1).pop()
-      statusP.innerHTML = RHOCore.toRholang(lastDatum)
-    })
-    .catch((oops) => {
-      console.log({ oops });
-    })
   })
 
 
