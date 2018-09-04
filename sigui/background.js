83f5a460b7b9a47390f996414d5c5b09213db3fd
Dan Connolly <dckc@madmode.com>
2018-09-04 10:27:35 -0500

    sigui: prune background.js dead code

--- sigui/background.js
+++
@@ -1,9 +0,0 @@
-// combination of rchain domain and randomly chosen data.
-const THE_TOKEN = 'rchain.coop/6kbIdoB2';
-
-chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-  console.log('rchain bg got message:', data);
-  if (!(data && data.type && data.type === THE_TOKEN)) { return; }
-  console.log(`got ${THE_TOKEN}`);
-  sendResponse({ success: true });
-});
