ea5d40cc7aba7d74dda776a078f4e4db8dc7ee9f
Dan Connolly <dckc@madmode.com>
2018-09-05 15:22:35 -0500

    oops: restore link from RhoTypes.js to global protobuf

--- sigui/lib/RhoTypes.js
+++ sigui/lib/RhoTypes.js
@@ -1,5 +1,6 @@
 /*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
-import * as $protobuf from "protobufjs/minimal";
+
+const $protobuf = protobuf; // ISSUE: global. but powerless, right?
 
 const $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
 
