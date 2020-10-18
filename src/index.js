7eba55e7f3ef23c4af402c89ff659e392d4afc50
Dan Connolly <dckc@madmode.com>
2020-10-18 16:18:00 -0500

    require selector match in mount

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import htm from 'htm';
 import m from 'mithril';
 import statusPage from './page.js';
 
+/**
+ * @param {T | null | undefined } x
+ * @returns { T }
+ * @template T
+ */
+function the(x) {
+  if (x === null || x === undefined)
+    throw new TypeError('BUG: unexpected null / undefined');
+  return x;
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const $ = (selector) => document.querySelector(selector);
   // const $$ = (selector) => document.querySelectorAll(selector);
   statusPage({
     $,
-    mount: (selector, component) => m.mount($(selector), component),
+    mount: (selector, component) => m.mount(the($(selector)), component),
     html: htm.bind(m),
     localStorage,
     fetch,
